72d4c689e4dc52cbe01124cf2bbf5864bb5bb191
EhoodGoren <ehoodgoren@gmail.com>
2021-11-22 21:21:21 +0200

    After each type in the input field, only the matching countries are displayed.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Country from "./Country";
 import { data } from './data';
 
 export default function App() {
-    const [country, setCountry] = useState('');
     const [inputValue, setInputValue] = useState('');
+    const [countryList, setCountryList] = useState([]);
 
     useEffect(() => {
-        setInputValue(country)
-    },[country]);
+        const matchingCountriesList = data.filter(country => {
+            return country.name.toLowerCase().includes(inputValue.toLowerCase());
+        })
+        console.log(matchingCountriesList);
+        setCountryList(matchingCountriesList);
+    },[inputValue])
+
+    useEffect(() => {
+        setCountryList(data);
+    },[])
+
     return (
         <div>
             <input onChange={e => setInputValue(e.target.value)} value={inputValue} placeholder='Search a country...' />
             <button>X</button>
             <button>V</button>
-            {data.map(country => (
-                <Country onClick={() => setCountry(country.name)} name={country.name} code={country.code}/>
+            {countryList.map(country => (
+                <Country onClick={() => setInputValue(country.name)} name={country.name} code={country.code}/>
             ))}
         </div>
     )
